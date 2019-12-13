66a3eca698b8d421b11644e30fdd17250833db7e
siddhartha <siddverma1999@gmail.com>
2019-12-13 21:26:22 +0530

    made static app, next step: adding functionality

--- lib/main.dart
+++ lib/main.dart
@@ -33,27 +33,153 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
       //padding: const EdgeInsets.all(6),
       child: Column(
         children: <Widget>[
-          Container(
-            alignment: Alignment.centerRight,
-            padding: EdgeInsets.symmetric(horizontal: 8,vertical: 2),
-            child: Text(
-              '0',
-              style: Theme.of(context).textTheme.display1,
-            ),
+          SizedBox(
+            height: 20,
           ),
+          Output(Theme.of(context).textTheme.display1),
           SizedBox(
             height: 5,
           ),
-          Container(
-            alignment: Alignment.centerRight,
-            padding: EdgeInsets.symmetric(horizontal: 8,vertical: 2),
-            child: Text(
-              '0',
-              style: Theme.of(context).textTheme.display3,
-            ),
-          ),
+          Output(Theme.of(context).textTheme.display3),
+          Expanded(child: Divider()),
+          BottomTable(),
         ],
       ),
     );
+  }
+}
+
+class Output extends StatelessWidget {
+  final TextStyle textStyle;
+  Output(this.textStyle);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerRight,
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+      child: Text(
+        '0',
+        style: textStyle,
+      ),
+    );
+  }
+}
+
+class BottomTable extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Container(
+          width: MediaQuery.of(context).size.width * 0.80,
+          child: Table(children: [
+            TableRow(children: [
+              CalcButtons(
+                  buttonText: '+', color: Theme.of(context).primaryColor),
+              CalcButtons(
+                  buttonText: '-', color: Theme.of(context).primaryColor),
+              CalcButtons(
+                  buttonText: '×', color: Theme.of(context).primaryColor),
+            ]),
+            TableRow(children: [
+              CalcButtons(
+                  buttonText: '7', color: Theme.of(context).primaryColor),
+              CalcButtons(
+                  buttonText: '8', color: Theme.of(context).primaryColor),
+              CalcButtons(
+                  buttonText: '9', color: Theme.of(context).primaryColor),
+            ]),
+            TableRow(children: [
+              CalcButtons(
+                  buttonText: '4', color: Theme.of(context).primaryColor),
+              CalcButtons(
+                  buttonText: '5', color: Theme.of(context).primaryColor),
+              CalcButtons(
+                  buttonText: '6', color: Theme.of(context).primaryColor),
+            ]),
+            TableRow(children: [
+              CalcButtons(
+                  buttonText: '1', color: Theme.of(context).primaryColor),
+              CalcButtons(
+                  buttonText: '2', color: Theme.of(context).primaryColor),
+              CalcButtons(
+                  buttonText: '3', color: Theme.of(context).primaryColor),
+            ]),
+            TableRow(children: [
+              CalcButtons(
+                  buttonText: '.', color: Theme.of(context).primaryColor),
+              CalcButtons(
+                  buttonText: '0', color: Theme.of(context).primaryColor),
+              CalcButtons(
+                  buttonText: '( )', color: Theme.of(context).primaryColor),
+            ]),
+          ]),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width * 0.20,
+          child: Table(
+            children: [
+              TableRow(children: [
+                CalcButtons(
+                    buttonText: '÷', color: Theme.of(context).primaryColor),
+              ]),
+              TableRow(children: [
+                CalcButtons(
+                    buttonText: 'AC', color: Theme.of(context).primaryColor),
+              ]),
+              TableRow(children: [
+                CalcButtons(
+                    buttonText: 'C', color: Theme.of(context).primaryColor),
+              ]),
+              TableRow(children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  child: CalcButtons(
+                      buttonText: '=',
+                      color: Theme.of(context).accentColor,
+                      textColor: Colors.grey[900]),
+                )
+              ]),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CalcButtons extends StatefulWidget {
+  final String buttonText;
+  final Color color;
+  final Color textColor;
+  CalcButtons(
+      {@required this.buttonText, @required this.color, this.textColor});
+
+  @override
+  _CalcButtonsState createState() => _CalcButtonsState();
+}
+
+class _CalcButtonsState extends State<CalcButtons> {
+  void _performCalculation() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: MediaQuery.of(context).size.height * 0.1,
+        child: MaterialButton(
+          elevation: 0,
+          color: widget.color,
+          child: Text(widget.buttonText,
+              style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.w300,
+                  color: (widget.textColor == null)
+                      ? Colors.white
+                      : widget.textColor)),
+          onPressed: _performCalculation,
+        ));
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:dsc_calc/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(SimpleCalculator());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
